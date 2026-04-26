c958ffc2302f86c69039b3ab647a6d60be3e1cc2
Kaustubh3117 <kaustubh3117@gmail.com>
2026-04-26 20:19:16 +0530

    fixed issue for catalog view for smaller screens

--- src/user/view/Catalog/catalogHelper.js
+++ src/user/view/Catalog/catalogHelper.js
@@ -1,3 +1,5 @@
+import { PrimeBadge } from "../../../shared/common/PrimeBadge/PrimeBadge";
+
 export const catalogs = (data) => {
     return [
         { id: 1, title: 'Products ', data: data?.products, catalogType: "products" },
@@ -24,4 +26,37 @@ export const btnData = (catalogType, navigate) => {
         tooltipOptions: { position: "left" },
         onClick: () => { navigate(`catalogView/${catalogType}`) }
     }
-} 
+}
+
+export const cardContent = (item) => {
+    const header = (
+        <div className="product-image-wrapper">
+            <img
+                src={item.image}
+                alt={item.image}
+                className="product-image"
+            />
+            <div className="product-image-overlay">
+                <i className="pi pi-eye" />
+            </div>
+        </div>
+    );
+
+    const content = (
+        <div className="-mt-4 -ml-3">
+            <span style={{ color: '#999', textDecoration: 'line-through' }}>₹{item.old_price}</span>
+            <div className='flex justify-content-start gap-2'>
+                <span className="text-lg font-weight-bold" style={{ color: '#1a7f3c' }}>₹{item.new_price}</span>
+                <PrimeBadge value={`${item.discount}% OFF`} severity="warning" className="text-sm" />
+            </div>
+        </div>
+    );
+
+    const title = <h6 className="-mt-3 -ml-3 white-space-nowrap overflow-hidden text-overflow-ellipsis">{item.title}</h6>
+
+    return {
+        header: header,
+        title: title,
+        subTitle: item?.new_price ? content : undefined,
+    }
+}
