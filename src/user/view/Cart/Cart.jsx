549ecafe9cb061a2347e42f2b27b6940958728e5
Kaustubh3117 <kaustubh3117@gmail.com>
2026-05-02 11:25:34 +0530

    Fixed dsign for smaller screen

--- src/user/view/Cart/Cart.jsx
+++ src/user/view/Cart/Cart.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Button } from "primereact/button";
 import { InputNumber } from "primereact/inputnumber";
 import { PrimeCard } from "../../../shared/common/PrimeCard/PrimeCard";
-import { Message } from "primereact/message";
+import { Chip } from 'primereact/chip'
 import { PrimeBadge } from "../../../shared/common/PrimeBadge/PrimeBadge";
+import "../Cart/cartStyle.css"
 
 export function Cart() {
     const products = [
@@ -15,13 +16,22 @@ export function Cart() {
 
     const cardContent = (product) => (
         <div className="grid">
-            <div className="col-3">
+            <div className="col-4 sm:col-3">
                 <img
                     src={"https://picsum.photos/300/200?random=70"}
                     alt={"https://picsum.photos/300/200?random=70"}
-                    className="product-image"
-                    style={{ width: "160px", height: "100px" }}
+                    className="responsive-img"
                 />
+
+            </div>
+            <div className="col-8 sm:col-4 text-center -mt-4 sm:mt-0">
+                <h4>{product.name}</h4>
+                <div>
+                    <PrimeBadge value={`50% OFF`} severity="warning" className="text-sm" />
+                    <span className="ml-2" style={{ color: '#999', textDecoration: 'line-through' }}>₹10</span>
+                    <span className="text-lg font-weight-bold ml-2" style={{ color: '#1a7f3c' }}>₹20</span>
+                </div>
+                <p>Delivered By Mon May 5 </p>
                 <InputNumber
                     value={product.qty}
                     showButtons
@@ -33,70 +43,60 @@ export function Cart() {
                     min={0}
                     className="custom-inputnumber mt-2"
                 />
-            </div>
-            <div className="col-4 text-center">
-
-                <h4>{product.name}</h4>
-                <div className="">
-                    <PrimeBadge value={`50% OFF`} severity="warning" className="text-sm" />
-                    <span style={{ color: '#999', textDecoration: 'line-through' }}>₹10</span>
-                    <span className="text-lg font-weight-bold" style={{ color: '#1a7f3c' }}>₹20</span>
-                </div>
-                <p>Delivered By Mon May 5 </p>
             </div >
-            <div className="col-2 flex align-items-center -mr-0">
-            </div>
         </div >
     )
 
     const productFooter = (
         <div className="grid">
             <div className="col">
-                <Button label="Buy Now" className="p-button-success w-full" />
+                <Button label="Buy Now" text raised className="w-full" />
             </div>
             <div className="col">
-                <Button icon="pi pi-trash" label="Remove" className="p-button-danger w-full" />
+                <Button severity="secondary" className="w-full" label="Remove" text raised />
             </div>
+
         </div>
     )
 
     // subtotal conent
     const subTotalCardContent = (
         <>
-            <div className="flex justify-between mb-2">
-                <span>Price</span>
+            <div className="flex justify-between mb-2 gap-2">
+                <span>Price:</span>
                 <span>₹{subtotal}</span>
             </div>
-            <div className="flex justify-between mb-2">
-                <span>Discount</span>
+            <div className="flex justify-between mb-2 gap-2">
+                <span>Discount: </span>
                 <span>₹{(subtotal * 0.05).toFixed(2)}</span>
             </div>
-            <Message
-                severity="success"
-                text={`Total Amount: ₹${(subtotal * 1.05).toFixed(2)}`}
-                className="w-full mt-4 -mb-4"
-            />
+            <div className="flex justify-between mb-2 gap-2">
+                <span>Platform Fees: </span>
+                <span>₹{subtotal}</span>
+            </div>
         </>
     )
 
     const subTotalFooter = (
-        <>
-            <div className="border-top-1 border-dashed border-gray-300 my-3"></div>
-            <Button label="Place Order" className="p-button-success mt-2 w-full" />
-        </>
+        <div className="flex align-items-center sm:static fixed bottom-0 left-0 right-0 z-5">
+            <Chip label={`Total Amount: ₹${(subtotal * 1.05).toFixed(2)}`} className="border-noround w-full h-3rem" />
+            <Button
+                label="Place Order"
+                className="p-button-success border-noround h-3rem w-8"
+            />
+        </div>
     )
 
     const subTotalTitle = (
         <>
             <h3>Price Details</h3>
-            <div className="border-top-1 border-dashed border-gray-300 my-3"></div>
         </>
     )
 
     return (
         <div className="grid">
             {/* left col */}
-            <div className="col-8">
+            <div className="col-12 sm:col-8">
                 <div>
                     {products.map((product, index) => (
                         <PrimeCard
@@ -110,7 +110,7 @@ export function Cart() {
             </div>
 
             {/* left col */}
-            <div className="col-4 mt-2">
+            <div className="col-12 sm:col-4 mt-2">
                 <PrimeCard title={subTotalTitle} content={subTotalCardContent} footer={subTotalFooter} />
             </div>
         </div>
